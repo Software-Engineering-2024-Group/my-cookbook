90e0c7f2cd8bc1bbb137819be2b9cdbd9450bd48
Mugdha <mugdhajoshi225@gmail.com>
2024-11-21 21:20:54 -0500

    adding frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,6 @@
-/*
-
-Copyright (C) 2022 SE CookBook - All Rights Reserved
-You may use, distribute and modify this code under the
-terms of the MIT license.
-You should have received a copy of the MIT license with
-this file. If not, please write to: help.cookbook@gmail.com
-
-*/
-
-/**
- * File name: App.tsx
- * Task - This is the parent component of the application. It creates the basic UI skeleton
- * viz the header, the search component and the app contents
- * Header and Search component remain static and app contents change according to the state of the application
- * @author Priyanka Ambawane - dearpriyankasa@gmail.com
- */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import applicationStore from './store';
 import './App.css';
 import GetIngredients from './features/GetIngredients/GetIngredients';
@@ -26,41 +9,63 @@ import AppContent from './features/AppContent/AppContent';
 import GetTags from './features/AppContent/Tag/GetTags';
 import CustomizedAccordions from './features/AppContent/NutritionFilter/CustomizedAccordions';
 import { ThemeProvider, useTheme } from './features/Themes/themeContext';
+import Login from './features/AppContent/Auth/Login'; // Import your Login component
+import Signup from './features/AppContent/Auth/Signup'; // Import your Signup component
 
 const store = applicationStore();
 
-// Separate function for the main application content
-const AppContentLayout: React.FC = () => {
-  const { theme } = useTheme();
-
-  return (
-    <div className="App" style={{ backgroundColor: theme.background, color: theme.color }}>
-      <div className="App-header" data-testid="header-comp-43" style={{ backgroundColor: theme.background }}>
-        <Header />
-      </div>
-      <div className="search-helper" data-testid="search-comp-43" style={{ backgroundColor: theme.background }}>
-        <GetIngredients />
-      </div>
-      <div className="search-helper" data-testid="header-comp-44" style={{ backgroundColor: theme.background }}>
-        <GetTags />
-      </div>
-      <div className="search-helper" data-testid="header-comp-45" style={{ backgroundColor: theme.background }}>
-        <CustomizedAccordions />
-      </div>
-      <div className="App-body" data-testid="body-comp-43" style={{ backgroundColor: theme.background }}>
-        <AppContent />
-      </div>
-    </div>
-  );
-};
-
 // Main App component
 const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  // Logic to check if the user is logged in
+  useEffect(() => {
+    const token = localStorage.getItem('authToken'); // Check for auth token in localStorage
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  // Layout for authenticated content
+  const AppContentLayout: React.FC = () => {
+    const { theme } = useTheme();
+
+    return (
+      <div className="App" style={{ backgroundColor: theme.background, color: theme.color }}>
+        <div className="App-header" data-testid="header-comp-43" style={{ backgroundColor: theme.background }}>
+          <Header />
+        </div>
+        <div className="search-helper" data-testid="search-comp-43" style={{ backgroundColor: theme.background }}>
+          <GetIngredients />
+        </div>
+        <div className="search-helper" data-testid="header-comp-44" style={{ backgroundColor: theme.background }}>
+          <GetTags />
+        </div>
+        <div className="search-helper" data-testid="header-comp-45" style={{ backgroundColor: theme.background }}>
+          <CustomizedAccordions />
+        </div>
+        <div className="App-body" data-testid="body-comp-43" style={{ backgroundColor: theme.background }}>
+          <AppContent />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <ThemeProvider>
-          <AppContentLayout />
+          <Routes>
+            {/* Route for Login page */}
+            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<AppContentLayout />} />
+            {/* Conditional route for Home page (after login) */}
+            <Route
+              path="/"
+              element={isLoggedIn ? <AppContentLayout /> : <Login setIsLoggedIn={setIsLoggedIn} />}
+            />
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
     </Provider>
